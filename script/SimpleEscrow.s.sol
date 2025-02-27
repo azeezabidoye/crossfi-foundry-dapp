aaddb1ef2b0ebff42731226fc3de688ffab30990
azeezabidoye <azeezabidoye@yahoo.com>
2025-02-27 14:54:31 +0100

    Deploy Script Updated

--- script/SimpleEscrow.s.sol
+++ script/SimpleEscrow.s.sol
@@ -7,7 +7,7 @@ import "../src/SimpleEscrow.sol";
 contract DeployEscrow is Script {
     function run() external {
         address seller = 0x3e61b7C58cb5D0d5F75082b758454a43676C8491;
-        uint256 depositAmount = 1 ether;
+        uint256 depositAmount = 2 ether;
 
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         SimpleEscrow escrow = new SimpleEscrow{value: depositAmount}(seller);
