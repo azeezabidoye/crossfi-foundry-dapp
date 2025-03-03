faafe955a3f57e14cfc3a012d969ca1baa63a72d
azeezabidoye <azeezabidoye@yahoo.com>
2025-03-03 13:52:20 +0100

    Deploy Script Updated

--- script/SimpleEscrow.s.sol
+++ script/SimpleEscrow.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.23;
 
 import "forge-std/Script.sol";
-import {SimpleEsc} from "../src/SimpleEscrow.sol";
+import {SimpleEs} from "../src/SimpleEscrow.sol";
 
 contract DeployEscrow is Script {
     function run() external {
